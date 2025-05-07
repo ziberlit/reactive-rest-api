1b23e027f6c716d359edc071e3bc53e330862818
Arye Leonid Ziberlit <ziberlit@gmail.com>
2025-05-07 19:22:12 +0300

    initial commit

---
+++ src/test/java/com/leonid/reactive_rest_api/ReactiveRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonid.reactive_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
